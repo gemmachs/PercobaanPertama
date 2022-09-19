bacec7a6a868ee460e4d3cc78237d62a3fb9b764
gemmachs <gemmacahya@gmail.com>
2022-09-19 09:57:54 +0700

    Edit Login Form sementara

--- app/src/main/java/id/ac/ipb/apps/percobaanpertama/LoginActivity.java
+++ app/src/main/java/id/ac/ipb/apps/percobaanpertama/LoginActivity.java
@@ -1,3 +1,42 @@
 package id.ac.ipb.apps.percobaanpertama;
-   class LoginActivity   {
+
+import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+public class LoginActivity extends AppCompatActivity {
+
+    String username = "admin";
+    String password = "admin";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final EditText txtUser = findViewById(R.id.txtUser);
+        final EditText txtPass = findViewById(R.id.txtPass);
+        Button login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(txtUser.getText().toString().equalsIgnoreCase(username)&& txtPass.getText().toString().equalsIgnoreCase(password)) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                } else{
+                    Toast.makeText(LoginActivity.this, "Username/Password Salah", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 }
---
+++ app/src/main/java/id/ac/ipb/apps/percobaanpertama/User.java
@@ -0,0 +1,4 @@
+package id.ac.ipb.apps.percobaanpertama;
+
+class User {
+}
